dcf7be21d783d9f5a67e2e8219a60c473216fb18
Александр Клонов <aleksandrklonov@MacBook-Air-Aleksandr.local>
2022-01-09 19:49:01 +0300

    finaly # Please enter the commit message for your changes. Lines starting

--- src/repository/post.js
+++ src/repository/post.js
@@ -52,15 +52,8 @@ export default new class PostsRepository {
     }
 
     getRelated(user, thread, forum, id) {
-        let selectQuery = `SELECT posts.id AS pid,
-        posts.parent_id AS post_parent,
-        posts.thread_id AS post_thread,
-        posts.message AS post_message,
-        posts.edited AS post_is_edited,
-        posts.created AS post_created,posts.forum AS post_forum,
-        posts.author AS post_author,`;
-
         let joinQuery = ' FROM posts ';
+        let selectQuery = `SELECT `;
 
         if (user != null) {
             joinQuery += 'JOIN users U ON U.nickname = posts.author ';
@@ -68,19 +61,27 @@ export default new class PostsRepository {
         }
         // тред
         if (thread != null) {
+            joinQuery += 'JOIN threads ON threads.id = posts.thread_id ';
             selectQuery += `threads.author AS thread_author,
             threads.created AS thread_created,threads.votes AS thread_votes,
             threads.id AS thread_id,
             threads.title AS thread_title,
             threads.message AS thread_message,threads.slug AS thread_slug,
             threads.forum AS thread_forum,`;
-            joinQuery += 'JOIN threads ON threads.id = posts.thread_id ';
         }
         // форум
         if (forum != null) {
             joinQuery += 'JOIN forums F ON F.slug = posts.forum ';
             selectQuery += 'F.slug AS f_forum, F.threads AS f_threads, F.title as f_title, F.posts AS f_posts, F."user" AS f_user,';
         }
+        selectQuery += `posts.id AS pid,
+        posts.parent_id AS post_parent,
+        posts.thread_id AS post_thread,
+        posts.message AS post_message,
+        posts.edited AS post_is_edited,
+        posts.created AS post_created,posts.forum AS post_forum,
+        posts.author AS post_author,`;
+        
         joinQuery += ` WHERE posts.id = $$${id}$$`;
         const text = selectQuery.slice(0, -1) + joinQuery;
 
